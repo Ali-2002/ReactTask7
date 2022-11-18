537d3f2e8d94114b11201bb55307758ff9c1cce5
Ali-2002 <ali.aliquliyev@inbox.ru>
2022-11-18 23:03:46 +0400

    finishedd

--- ReactTask7/src/App.jsx
+++ ReactTask7/src/App.jsx
@@ -1,11 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 import Form from './components/Form/Form'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
         <Form/>
--- ReactTask7/src/components/Form/Form.jsx
+++ ReactTask7/src/components/Form/Form.jsx
@@ -1,6 +1,5 @@
-import { useEffect } from "react";
 import { React, useState } from "react";
-import "./Form.css";
+import "./Form.css"
 const Form = () => {
   const [state, setState] = useState({
     firstname: "",
@@ -10,63 +9,60 @@ const Form = () => {
     comments: "",
     isActive: false,
   });
+  const validate = () => {
+    if (
+      state.firstname &&
+      state.lastname &&
+      state.email &&
+      state.phone &&
+      state.comments
+    ) {
+      return true;
+    }
+    return false;
+  };
 
   const handleChange = (e) => {
-    setState({ ...state, [e.target.name]: e.target.value });
+    validate()
+      ? setState({ ...state, [e.target.name]: e.target.value, isActive: true })
+      : setState({
+          ...state,
+          [e.target.name]: e.target.value,
+          isActive: false,
+        });
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(state);
     alert("Proses uğurla yerinə yetirildi");
     console.log("Adı: " + state.firstname);
     console.log("Soyadı: " + state.lastname);
     console.log("Email ünvanı: " + state.email);
     console.log("Telefon nömrəsi: " + state.phone);
     console.log("Şərh: " + state.comments);
-   
   };
-
-  useEffect(()=> {
-    if (
-        state.firstname !== "" &&
-        state.lastname !== "" &&
-        state.email !== "" &&
-        state.phone !== "" &&
-        state.comments !== ""
-      ) {
-        setState({...state, isActive: true });
-  
-      } else {
-        setState({...state, isActive: false });
-      }
-  },[state])
 
   return (
     <>
-      <main> 
+      <main>
         <form className="user-form" onSubmit={handleSubmit}>
           <label htmlFor="firstname">
             Ad:
             <input
-              className="full-name"
               name="firstname"
               type="text"
               onChange={handleChange}
               value={state.firstname}
-              required
             />
           </label>
 
           <label htmlFor="lastname">
             Soyad:
             <input
-              className="full-name"
               name="lastname"
               type="text"
               onChange={handleChange}
               value={state.lastname}
-              required
             />
           </label>
 
